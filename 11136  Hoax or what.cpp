7c2d406537ce99f93d34839b756c6a8fc9a1df93
Swapnil <swpnltiwari361@gmail.com>
2017-06-13 17:20:46 +0530

    11136 Hoax or what
    11136 Hoax or what

---
+++ 11136  Hoax or what.cpp
@@ -0,0 +1,42 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+#define FOR(i, a, b, ch) for(int i=a;i<b;i+=ch)
+#define REP(i, a, b) for(int i=a, i<b;++i)
+//#define ITR(i, a, b) for(it )
+#define inp(t) cin>>t;
+#define out(t)  cout<<t<<endl;
+#define db(ds)   cout<<ds<<" ";
+#define all(arr)    arr.begin(), arr.end()
+typedef  long long ll;
+#define  INF 999999
+#define MOD 1000000007
+#define fast ios::sync_with_stdio(false);
+
+int main() {
+    fast;
+    int days;
+    inp(days);
+    while (days) {
+        int n, x;
+        ll ans = 0;
+        multiset<int> msi;
+        multiset<int>::iterator itrb, itre;
+        for (int i = 0; i < days; ++i) {
+            inp(n);
+            for (int j = 0; j < n; ++j) {
+                inp(x);
+                msi.insert(x);
+            }
+            itrb = msi.begin();
+            itre = msi.end();
+            --itre;
+            ans += *itre - *itrb;
+            msi.erase(itrb);
+            msi.erase(itre);
+        }
+        out(ans);
+        inp(days);
+    }
+    return 0;
+}
